3ee126303007039005154b86546cb41337332eb3
Hemantcodes <hy564636@gmail.com>
2026-02-17 22:16:29 +0530

    done leetcode daily problem Binary Watch

---
+++ LeeetCode/Day3/dailyLeetcode(401)/1.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def readBinaryWatch(self, turnedOn):
+        """
+        :type turnedOn: int
+        :rtype: List[str]
+        """
+        if turnedOn > 8: return []
+        ans = []
+        for hour in range(12):
+            for minute in range(60):
+                if hour.bit_count() + minute.bit_count() == turnedOn:
+                    ans.append(f"{hour}:{minute:02d}")
+
+        return ans           
