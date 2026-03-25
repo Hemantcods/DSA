c6828c91b3e681594badddbe057f234ae797ce2e
hemant <hy564636@gmail.com>
2026-03-25 11:26:30 +0530

    Enhance linked list concepts by updating DoublyLL and adding CircularLL implementation. jssfxzvxy.

--- Concept/Day 6/CircularLL.py
+++ Concept/Day 6/CircularLL.py
@@ -10,30 +10,40 @@ class DoublyLL:
         temp=Node(value)
         if(self.head==None):
             self.head=temp
+            self.head.next=self.head
+            self.head.prev=self.head
         else:
             t=self.head
-            while(t.next!=None):
+            while(t.next!=self.head):
                 t=t.next
             t.next=temp
+            temp.next=self.head
             temp.prev=t
+            self.head.prev=temp
     def PrintDLL(self):
         t=self.head
-        while(t.next!=None):
+        if self.head==None:
+            print("the LL is empty")
+        while(t.next!=self.head):
             print(t.data,end=" ")
             t=t.next
         print(t.data)
     def insertAtBegning(self,value):
         temp=Node(value)
         if(self.head==None):
-            self.head=Node
+            self.head = temp
+            temp.next = temp
+            temp.prev = temp
             return
         temp.next=self.head
-        self.head.prev=temp
+        temp.prev=self.head.prev
+        self.head.prev.next=temp
         self.head=temp
+        self.head.prev=temp
     def insertAtMid(self,value,target):
         temp=Node(value) 
         t=self.head
-        while(t.next!=None):
+        while(t.next!=self.head):
             if(t.data==target):
                 temp.next=t.next
                 temp.prev=t
@@ -43,23 +53,30 @@ class DoublyLL:
         if(t.data==target):
             temp.prev=t
             t.next=temp
+            temp.next=self.head
+            self.head.prev=temp
     def deleteDLL(self,value):
         if self.head==None:
             print("Nothing to delete")
             return
         t=self.head
-        if (t.data==value):
-            self.head=t.next
-            self.prev=None
+        if t.next == self.head and t.data == value:
+            self.head = None
             return
-        while t.next !=None:
+        if (t.data==value):
+            t.next.prev=self.head.prev
+            self.head=t.next
+            self.head.prev.next=self.head
+            return
+        while t.next !=self.head:
             if(t.data==value):
                 t.prev.next=t.next
                 t.next.prev=t.prev
                 return
             t=t.next
         if(t.data==value):
-            t.prev.next=None 
+            t.prev.next=t.next
+            self.head.prev=t.prev
 
                 
 
