297434bc1c7c061b2da99e3b87dae1e3386f6cf0
hemant <hy564636@gmail.com>
2026-04-22 00:27:46 +0530

    Solved LeetCode 1722. Implemented solution for Minimize Hamming Distance After Swap Operations, improving daily progress.

--- LeeetCode/Day64/1722. Minimize Hamming Distance After Swap Operations.py
+++ LeeetCode/Day64/1722. Minimize Hamming Distance After Swap Operations.py
@@ -1,24 +1,38 @@
+from collections import defaultdict,Counter
 class Solution:
     def minimumHammingDistance(self, source: List[int], target: List[int], allowedSwaps: List[List[int]]) -> int:
-        cur=self.difference(source,target)
-        if cur==0:
-            return 0
-        for swp in allowedSwaps:
-            swaped=self.swap(source,swp)
-            new=self.difference(swaped,target)
-            if new<=cur:
-                source=swaped
-                cur=new
-            if new==0:
-                return 0
-        return cur                    
-
-    def difference(self,source,target):
-        res=0
-        for i in range(len(source)):
-            if source[i]!=target[i]:
-                res+=1
-        return res                
-    def swap(self,source,swap):
-        source[swap[0]],source[swap[1]]=source[swap[1]],source[swap[0]]  
-        return source  
+        n=len(source)
+        rank=[0]*n
+        parent=[-1]*n
+        def Find(x):
+            if parent[x]!=x:
+                parent[x]=Find(parent[x])
+            return parent[x] 
+        def Union(x,y):
+            rootX=Find(x)
+            rootY=Find(y)
+            if rootX==rootY: return
+            if rank[rootX]<rank[rootY]:
+                rootX,rootY=rootY,rootX
+            parent[rootY]=rootX
+            if rank[rootX]==rank[rootY]:
+                rank[rootX]+=1
+            
+        for i in range(n):
+            parent[i]=i
+        # Union of allowed swaps
+        for elem in allowedSwaps:
+            Union(elem[0],elem[1])
+        # unordered map for storing frequency and indexes
+        mp = defaultdict(Counter)  
+        for i in range(n):
+            root = Find(i)
+            mp[root][source[i]] += 1
+        res = 0
+        for i in range(n):
+            root = Find(i)
+            if mp[root][target[i]] > 0:
+                mp[root][target[i]] -= 1
+            else:
+                res += 1
+        return res
